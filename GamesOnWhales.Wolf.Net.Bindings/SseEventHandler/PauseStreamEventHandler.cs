38cbb399f961cd7c52a080b043c2f019b9c2155d
Sebastian Sommer <mail@sommer-sebastian.de>
2026-03-16 17:55:19 +0100

    Added EventHandler for all previousely as method calls implemented cases, TODO: add classes for a typed response mapping.

---
+++ GamesOnWhales.Wolf.Net.Bindings/SseEventHandler/PauseStreamEventHandler.cs
@@ -0,0 +1,14 @@
+namespace GamesOnWhales;
+
+[SseEventHandler]
+public partial class PauseStreamEventHandler : ISseEventHandler<string>
+{
+    // Todo: Implement Convert to correct dto.
+    public Task Convert(string eventData, out string result)
+    {
+        result = eventData;
+        return Task.CompletedTask;
+    }
+
+    public string EventName => "wolf::core::events::PauseStreamEvent";
+}
---
+++ GamesOnWhales.Wolf.Net.Bindings/SseEventHandler/PlugDeviceEventHandler.cs
@@ -0,0 +1,19 @@
+namespace GamesOnWhales;
+
+// public class PlugDeviceEvent
+// {
+//     
+// }
+
+[SseEventHandler]
+public partial class PlugDeviceEventHandler : ISseEventHandler<string>
+{
+    // Todo: Implement Convert to correct dto.
+    public Task Convert(string eventData, out string result)
+    {
+        result = eventData;
+        return Task.CompletedTask;
+    }
+
+    public string EventName => "wolf::core::events::PlugDeviceEvent";
+}
