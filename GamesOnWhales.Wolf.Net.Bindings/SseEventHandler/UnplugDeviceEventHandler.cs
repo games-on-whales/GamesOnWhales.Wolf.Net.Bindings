2033f0d98d66a07a3571eb2223c15e6cdbe48509
Sebastian Sommer <mail@sommer-sebastian.de>
2026-03-20 16:47:49 +0100

    fixed event name

--- GamesOnWhales.Wolf.Net.Bindings/SseEventHandler/UnplugDeviceEventHandler.cs
+++ GamesOnWhales.Wolf.Net.Bindings/SseEventHandler/UnplugDeviceEventHandler.cs
@@ -3,5 +3,5 @@ namespace GamesOnWhales.SSE;
 [SseEventHandler]
 public partial class UnplugDeviceEventHandler : ISseEventHandler<string>
 {
-    public string EventName => "wolf::core::events::UnPlugDeviceEvent";
+    public string EventName => "wolf::core::events::UnplugDeviceEvent";
 }
